de1077676d586ed6afb0c34946c292fb3deae210
Judioo <contact@judioo.com>
2025-05-08 03:46:56 +0400

    added upload url

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'sanne.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'qjqqeunp2n.ufs.sh',
+      },
     ],
   },
   
